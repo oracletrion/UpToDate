c06847442f116e856c3c80d42471e83f2b91aca3
Vanessa Tan <vstan@ucsc.edu>
2018-03-14 15:17:04 -0700

    Fixed nav bar and index page

--- mysite/reddit/urls.py
+++ mysite/reddit/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.form, name='index'),
     path('form/', views.form, name='form'),
     path('search/', views.search, name='search'),
     path('<str:sub_name>/', views.reddit_test, name='reddit'),
